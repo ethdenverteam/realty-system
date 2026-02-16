db76cfbe4495792439263be407ea22ddd9b965d1
ethdenverteam <ethdenverteam@gmail.com>
2026-02-16 13:49:05 -0800

    update

--- migrations/versions/merge_chat_subscription_and_chat_group.py
+++ migrations/versions/merge_chat_subscription_and_chat_group.py
@@ -1,6 +1,6 @@
 """Merge chat subscription and chat group branches
 
-Revision ID: merge_chat_subscription_and_chat_group
+Revision ID: merge_subscription_chat_group
 Revises: ('add_subscription_chat_links', 'add_chat_group_category')
 Create Date: 2024-12-19 00:00:00.000000
 """
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = 'merge_chat_subscription_and_chat_group'
+revision = 'merge_subscription_chat_group'
 down_revision = ('add_subscription_chat_links', 'add_chat_group_category')
 branch_labels = None
 depends_on = None
